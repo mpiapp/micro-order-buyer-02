adb4a4d4940f07557f50bbe348dbe33aba04395e
Rokujima <officials.haruka@gmail.com>
2021-09-30 14:07:32 +0700

    RpcException TDD

--- src/utils/rpc-exception.filter.ts
+++ src/utils/rpc-exception.filter.ts
@@ -1,10 +1,10 @@
-import { Catch, RpcExceptionFilter, ArgumentsHost } from '@nestjs/common';
+import { Catch, RpcExceptionFilter } from '@nestjs/common';
 import { Observable, throwError } from 'rxjs';
 import { RpcException } from '@nestjs/microservices';
 
 @Catch(RpcException)
 export class ExceptionFilter implements RpcExceptionFilter<RpcException> {
-  catch(exception: RpcException, host: ArgumentsHost): Observable<any> {
+  catch(exception: RpcException): Observable<any> {
     return throwError(exception.getError());
   }
 }
